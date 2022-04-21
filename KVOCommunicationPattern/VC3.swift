c46e46b178c4cb6f35131909868c3d4ed775d9e5
DennisNewMachine <dnsljnv411@gmail.com>
2022-04-21 13:16:21 +0300

    Add one more button

--- KVOCommunicationPattern/VC3.swift
+++ KVOCommunicationPattern/VC3.swift
@@ -10,18 +10,37 @@ import UIKit
 class VC3: UIViewController, DismissButtonProtocol {
     typealias VCDismissProtocol = UIViewController
     var dismissButton: UIButton = UIButton()
+    var buttonForCommunication = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
-        setUpButton()
+        setUpDissmissButton()
+        setUpCommunicationButton()
     }
 }
 
+// buttons
 extension VC3 {
-    private func setUpButton() {
+    private func setUpDissmissButton() {
         addDismissButton(VC: self)
         dismissButton.addTarget(self, action: #selector(dismissButtonAction), for: .touchUpInside)
+    }
+    
+    private func setUpCommunicationButton() {
+        buttonForCommunication.layer.backgroundColor = UIColor.yellow.cgColor
+        buttonForCommunication.layer.cornerRadius = 20
+        buttonForCommunication.setTitle("Change color", for: .normal)
+        buttonForCommunication.setTitleColor(.black, for: .normal)
+        buttonForCommunication.setTitleColor(.orange, for: .highlighted)
+
+        view.addSubview(buttonForCommunication)
+        buttonForCommunication.translatesAutoresizingMaskIntoConstraints = false
+        buttonForCommunication.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
+        buttonForCommunication.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
+        buttonForCommunication.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        buttonForCommunication.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        buttonForCommunication.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
     
     @objc private func dismissButtonAction() {
